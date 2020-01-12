526b447910b1a58989a43497d7fe4721b057cb3c
Uzma Ferdaus <uferdaus@nisum.com>
2020-01-12 08:53:21 -0800

    initial commit 12-01-2020

---
+++ src/main/java/com/example/springbootproject/controller/Controller.java
@@ -0,0 +1,18 @@
+package com.example.springbootproject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class Controller {
+	
+	@RequestMapping()
+	public String Hello() {
+		
+		return "Hello World";
+	}
+	
+	
+
+}
