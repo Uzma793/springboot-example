8b86524ca5905c69e9e449d3f591d4067c3c8775
Uzma Ferdaus <uferdaus@nisum.com>
2020-01-21 02:59:15 -0800

    update main

--- src/main/java/com/example/springbootproject/SpringBootProjectApplication.java
+++ src/main/java/com/example/springbootproject/SpringBootProjectApplication.java
@@ -3,30 +3,23 @@ package com.example.springbootproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class SpringBootProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootProjectApplication.class, args);
-	
+
 	}
-	
+
 	public void get() {
 		System.out.println("get");
 	}
+
 	public void getall() {
 
 		System.out.println("getall");
-		}
-	
-	
-	
-	public String name() {
-		
-		return "name";
+		System.out.println("Hello");
 	}
+
 	
-
-
 }
